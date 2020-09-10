9283d95c6aa7e00d64edaca672846e0e17c5f402
Chandra <lchandratejareddy@gmail.com>
2020-09-10 10:09:52 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, Extension
+from setuptools import find_packages
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+if __name__ == "__main__":
+    setup(
+        name="colabcode",
+        version="0.0.6",
+        description="ColabCode - Run codeserver on Colab!",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author="Abhishek Thakur",
+        author_email="abhishek4@gmail.com",
+        url="https://github.com/abhishekkrthakur/colabcode",
+        license="MIT License",
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=["pyngrok>=4.1.11"],
+        platforms=["linux", "unix"],
+        python_requires=">3.5.2",
+    )
